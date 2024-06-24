54945b9f6d1589022ad5e1b7ea36b192885cba0c
Matthijs van Wijngaarden <Matthijsbbis@hotmail.com>
2024-06-24 10:40:35 +0200

    updates

--- scripts/assessment.js
+++ scripts/assessment.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // The questions that if answered with 'yes', will show the child questions
+    const questionDependencies = {
+        "1.1.5": ["1.1.6", "1.1.7", "1.2"]
+    };
+
+    var questionsAssociations = {};
+
     // Recursive function to render questions based on the tree structure
     function renderQuestions(container, questionsTree) {
         const ageGroup = sessionStorage.getItem('ageGroup');
 
         for (const key in questionsTree) {
+            questionsAssociations[key] = questionsTree[key].question;
+
             const questionData = questionsTree[key];
             const div = document.createElement('div');
             div.classList.add('question');
+            div.dataset.key = key; // Store the question key in data attribute
 
             if (questionData.type === "header") {
                 const header = document.createElement('h2');
@@ -14,35 +24,71 @@ document.addEventListener("DOMContentLoaded", function() {
                 div.appendChild(header);
             } else if (questionData.type === "subheader") {
                 const subheader = document.createElement('h3');
-                subheader.textContent = questionData.question.replace("children", ageGroup);
+                subheader.textContent = questionData.question.replace("children", ageGroup.toLowerCase());
                 div.appendChild(subheader);
             } else {
                 const label = document.createElement('label');
                 label.textContent = questionData.question;
                 div.appendChild(label);
 
-                let input;
+                let inputContainer;
                 if (questionData.type === "text") {
-                    input = document.createElement('textarea');
-                    input.rows = 5;
+                    inputContainer = document.createElement('textarea');
+                    inputContainer.rows = 5;
                 } else if (questionData.type === "number") {
-                    input = document.createElement('input');
-                    input.type = 'number';
+                    inputContainer = document.createElement('input');
+                    inputContainer.type = 'number';
                 } else if (questionData.type === "yesno") {
-                    input = document.createElement('select');
-                    const optionYes = document.createElement('option');
-                    optionYes.value = "yes";
-                    optionYes.textContent = "Yes";
-                    input.appendChild(optionYes);
+                    inputContainer = document.createElement('div');
 
-                    const optionNo = document.createElement('option');
-                    optionNo.value = "no";
-                    optionNo.textContent = "No";
-                    input.appendChild(optionNo);
+                    const labelYes = document.createElement('label');
+                    labelYes.textContent = 'Yes';
+                    labelYes.htmlFor = `${key}-yes`;
+
+                    const inputYes = document.createElement('input');
+                    inputYes.type = 'radio';
+                    inputYes.name = key;
+                    inputYes.value = 'yes';
+                    inputYes.id = `${key}-yes`;
+
+                    const labelNo = document.createElement('label');
+                    labelNo.textContent = 'No';
+                    labelNo.htmlFor = `${key}-no`;                    
+
+                    const inputNo = document.createElement('input');
+                    inputNo.type = 'radio';
+                    inputNo.name = key;
+                    inputNo.value = 'no';
+                    inputNo.id = `${key}-no`;
+
+                    const yesContainer = document.createElement('div');
+                    yesContainer.classList.add('radio-container');
+                    yesContainer.appendChild(labelYes);
+                    yesContainer.appendChild(inputYes);
+
+                    const noContainer = document.createElement('div');
+                    noContainer.classList.add('radio-container');
+                    noContainer.appendChild(labelNo);
+                    noContainer.appendChild(inputNo);
+
+                    inputContainer.appendChild(yesContainer);
+                    inputContainer.appendChild(noContainer);
+
+                    inputContainer.querySelectorAll('input').forEach(input => {
+                        input.addEventListener('change', function() {
+                            handleDependencies(key, input.value);
+                        });
+                    });
                 }
 
-                input.dataset.question = questionData.question; // Store the question text in data attribute
-                div.appendChild(input);
+                inputContainer.dataset.question = questionData.question; // Store the question text in data attribute
+                inputContainer.dataset.key = key; // Store the question key in data attribute
+                div.appendChild(inputContainer);
+            }
+
+            // Hide dependent questions initially
+            if (Object.values(questionDependencies).flat().includes(key)) {
+                div.style.display = "none";
             }
 
             container.appendChild(div);
@@ -52,21 +98,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 childContainer.classList.add('child-questions');
                 container.appendChild(childContainer);
 
-                if (questionData.type !== "header" && questionData.type !== "subheader") {
-                    input.addEventListener('keypress', function(event) {
-                        if (event.key === 'Enter' && !event.shiftKey) {
-                            event.preventDefault();
-                            // Render child questions only if there's an input value
-                            if (input.value.trim() !== "") {
-                                childContainer.innerHTML = ''; // Clear previous questions
-                                renderQuestions(childContainer, questionData.children);
-                            }
-                        }
-                    });
-                } else {
-                    renderQuestions(childContainer, questionData.children);
-                }
+                renderQuestions(childContainer, questionData.children);
             }
+        }
+    }
+
+    // Function to handle showing questions dependent on answers given in previous questions
+    function handleDependencies(questionKey, value) {
+        const questionsContainer = document.getElementById('questionsContainer');
+        const dependentQuestions = questionDependencies[questionKey];
+
+        if (dependentQuestions) {
+            dependentQuestions.forEach(depKey => {
+                const questionDiv = questionsContainer.querySelector(`[data-key="${depKey}"]`);
+                if (questionDiv) {
+                    if (value === "no") {
+                        questionDiv.style.display = "block";
+                    } else {
+                        questionDiv.style.display = "none";
+                    }
+                }
+            });
         }
     }
 
@@ -80,7 +132,15 @@ document.addEventListener("DOMContentLoaded", function() {
             // Collect answers and generate summary
             const answers = {};
             questionsContainer.querySelectorAll('textarea, input, select').forEach(input => {
-                if (input.value.trim() !== "") {
+                if (input.type === 'radio') {
+                    if (input.checked) {
+                        // Check if dataset.question exists, if not, take from questionsAssociations
+                        if (typeof input.dataset.question === "undefined") {
+                            questionId = input.id.split('-')[0]
+                            answers[questionsAssociations[questionId]] = input.value
+                        }
+                    }
+                } else if (input.value.trim() !== "") {
                     answers[input.dataset.question] = input.value;
                 }
             });
